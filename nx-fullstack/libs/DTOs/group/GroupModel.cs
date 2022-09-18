197fadb1b5b0f23986d4793cd28440c0b4e15253
13angs <13angz.um@gmail.com>
2022-09-18 15:26:30 +0000

    dockerize nx-fullstack api

--- nx-fullstack/libs/DTOs/group/GroupModel.cs
+++ nx-fullstack/libs/DTOs/group/GroupModel.cs
@@ -3,6 +3,6 @@ namespace NxFullstack.DTOs.group
     public class GroupModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
