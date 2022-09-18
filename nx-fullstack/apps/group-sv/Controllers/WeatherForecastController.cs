cef2171d6792b901cd02f21d6dded86ee26626be
13angs <13angz.um@gmail.com>
2022-09-18 03:30:21 +0000

    Setup nx-fullstack Debug env

--- nx-fullstack/apps/group-sv/Controllers/WeatherForecastController.cs
+++ nx-fullstack/apps/group-sv/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        var rng = new Random();
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
---
+++ nx-fullstack/libs/Models/BaseEntity.cs
@@ -0,0 +1,5 @@
+﻿namespace NxFullstack.Models;
+public class BaseEntity
+{
+    
+}
